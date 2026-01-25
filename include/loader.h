31b3777be24d581c85b043cb80905ff597b55743
Benitas <benitas987@gmail.com>
2026-01-25 00:33:47 +0000

    loading pre-trained model weights and back prop test

--- include/loader.h
+++ include/loader.h
@@ -3,3 +3,9 @@
 #include <vector>
 
 std::vector<LabeledImage> loadImages(const std::string& imPath, const std::string& lbPath, uint32_t maxSamples = 0);
+
+// path to binary file containing an array of float32 values. Size given in number of floats
+std::vector<float> load_floats(const std::string& path, int size);
+
+
+std::pair<std::vector<LabeledImage>, std::vector<LabeledImage>> load_train_test(size_t count = 0);
